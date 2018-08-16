95950d3e1ebf042ae9498c0b78385e1f37993135
sdlkasfadfsaf <42405837+sdlkasfadfsaf@users.noreply.github.com>
2018-08-16 06:34:00 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -39,24 +39,47 @@ client.on("guildMemberAdd", member => {
 })
 
 
-const invites = {};
-
-client.on('guildMemberAdd', member => {
-  member.guild.fetchInvites().then(guildInvites => {
-    const alp = invites[member.guild.id];
-    const inviter = client.users.get(invite.inviter.id);
-    if (msg.guild.channels.find('name', 'warns')) {
-     msg.guild.channels.find('name', 'warns').send(`
-	 
-	 <@${member.user.id}> 
-	 اهلا وسهلا بك في السيرفر
-	 
-	 ${message.guild.memberCount} اصبح عدد السيرفر 
-	 
-	 by <@${inviter.id}>`);
-          }
-        }
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
+}
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "Legendary Shop")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        })
+    })
 })
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.find('name', 'ask');
+    if (!channel) {
+        console.log("!channel fails");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('made it till here!');
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "Legendary Shop")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+                    console.log(3);
+                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
+ channel.send(`By :${Invite.inviter}`)            
+ }
+            dat[Inv] = Invite.uses;
+        })
+    })
+});
 
 
 
